11269ff62d2a4211ee5a34f7d3d56b5d3b08a7d5
Ana C <apspfc@gmail.com>
2021-09-06 16:08:42 +0100

    done

--- routes/drones.js
+++ routes/drones.js
@@ -1,36 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
+
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+const Drone = require("../models/Drone.model");
+
+router.get('/drones', async (req, res, next) => {
+ 
   // Iteration #2: List the drones
+  const drone = await Drone.find();
+  res.render("drones/list",{drone});
   // ... your code here
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
+  res.render("drones/create-form");
   // ... your code here
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/drones/create', async (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  await Drone.create({name, propellers, maxSpeed});
+  res.redirect("/drones");
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const drone = await Drone.findById(req.params.id);
+  res.render("drones/update-form", drone);
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
+  const {name, propellers, maxSpeed} = req.body;
+  await Drone.findByIdAndUpdate(req.params.id, {
+    name,
+    propellers,
+    maxSpeed,
+  });
+  res.redirect(`/drones`);
+
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/drones/:id/delete', async (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
+  await Drone.findByIdAndRemove(req.params.id);
+
+  res.redirect("/drones");
 });
 
 module.exports = router;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,22 @@
 // Iteration #1
+
+require("../db");
+const Drone = require("../models/Drone.model");
+
+
+const drones = [{
+    name: "Drone 1",
+    propellers: 4,
+    maxSpeed: 30,
+},
+{
+name: "Drone 2",
+propellers: 7,
+maxSpeed: 50,},
+{
+name: "Drone 3",
+propellers: 9,
+maxSpeed: 89,
+}];
+
+Drone.insertMany(drones).then((dronesFromDb)=> {console.log(`drones created - ${dronesFromDb.length}`)});
